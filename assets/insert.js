b4533adf1ec0d6ed55f86973bb4c56249bea374b
Julian Schmuckli <julian@schmuckli.net>
2018-11-13 16:04:04 +0100

    Optimized for iOS and Lighthouse audit

--- assets/insert.js
+++ assets/insert.js
@@ -1,9 +1,17 @@
 function showTemperature() {
     try {
-        var transaction = IndexDB.transaction(["weather-data"]).objectStore("weather-data").get("current").onsuccess = function (event) {
-            document.getElementById("temperature_1").innerText = Math.round(event.target.result.main.temp);
+        IndexDB.transaction(["weather-data"]).objectStore("weather-data").get("current").onsuccess = function (event) {
+            document.getElementById("temperature").innerText = Math.round(event.target.result.main.temp);
+            document.getElementById("humidity").innerText = Math.round(event.target.result.main.humidity);
+            document.getElementById("pressure").innerText = Math.round(event.target.result.main.pressure);
+        };
+
+        IndexDB.transaction(["last-used"]).objectStore("last-used").get("current").onsuccess = function (event) {
+            var date = new Date(event.target.result.ts);
+            document.getElementById("last_time_synced").innerText = (date.getDate() + "").padStart(2, "0") + "." + ((date.getMonth() + 1) + "").padStart(2, "0") + "." + date.getFullYear() + " " + (date.getHours() + "").padStart(2, "0") + ":" + (date.getMinutes() + "").padStart(2, "0") + ":" + (date.getSeconds() + "").padStart(2, "0");
         }
     } catch (e) {
+        console.error(e);
         getData("Baden,Switzerland", showTemperature);
     }
 }
